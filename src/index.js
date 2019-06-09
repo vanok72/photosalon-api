46f733d4441ffd0baa823f5a69d7b44f32760162
vanok72 <nevermoreagain72@gmail.com>
2019-06-09 18:52:50 +0300

    complite with login staff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import Promise from 'bluebird';
 
+import Auth from './Routes/Auth';
+
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
+app.use('/api/auth', Auth);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.listen(3000, () => console.log("Running on localhost:3000"));
+app.listen(3000, () => console.log('Running on localhost:3000'));
